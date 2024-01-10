c8e2c6ddca22497711de3eb237b48b24916cb15e
Rafael Ribeiro <stillrafael@pop-os.localdomain>
2024-01-10 19:39:10 -0300

    works with multiple words now

--- substrings.rb
+++ substrings.rb
@@ -1,7 +1,13 @@
 require 'pry-byebug'
 
 def substrings(strings, dictionary)
-    result = dictionary.filter_map{|dict_word|strings.include?(dict_word) ? dict_word : nil }
+    string_words = strings.split
+    result = Array.new
+    string_words.each do |word|
+        word = word.downcase
+        result.concat(dictionary.filter_map{|dict_word|word.include?(dict_word) ? dict_word : nil})
+    end
+    # result = dictionary.filter_map{|dict_word|strings.include?(dict_word) ? dict_word : nil }
     result.tally
         
 
@@ -9,4 +15,4 @@ end
 
 
 puts dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-puts substrings('below', dictionary)
+puts substrings('below low GO go firm', dictionary)
